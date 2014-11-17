2679cd799050cb1ec5ba58964249503e30afe2b4
Andreas Wurzer <andreas.wurzer@novum-ccservices.at>
2014-11-17 17:06:43 +0100

    added weekly working hours calc

--- src/workjeet.clj
+++ src/workjeet.clj
@@ -13,3 +13,24 @@
         header-row (first (row-seq sheet))
         columns (apply array-map (interleave (column-range :A :Z) (map read-cell header-row)))]
     (rest (select-columns columns sheet))))
+
+(defn get-week-of-year [date]
+  (.getWeekOfWeekyear (org.joda.time.LocalDate/fromDateFields date)))
+
+(defn get-duration [datetime]
+  (let [end (.toDateTime (org.joda.time.LocalDateTime/fromDateFields datetime))
+        start (.withTimeAtStartOfDay end)]
+    (org.joda.time.Duration. start end)))
+
+(defn sum-working-hours [week]
+  (let [zero-excel-date (org.joda.time.LocalDateTime/fromDateFields (org.apache.poi.ss.usermodel.DateUtil/getJavaDate 0.0))
+        duration-sum (.toPeriod (reduce #(.plus %1 %2) (map #(get-duration (get % "Dauer (rel.)")) week)))]
+    (.toDate (.plus zero-excel-date duration-sum))))
+
+(defn partition-by-week [timesheet]
+  (partition-by
+   #(get-week-of-year (get % "Datum"))
+   timesheet))
+
+(defn weekly-working-hours [timesheet]
+  (map sum-working-hours (partition-by-week timesheet)))
